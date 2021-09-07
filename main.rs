eb9ccf8b7d0c3faa083255c1a1ab0e8bd95b91f8
Shuhei <sh7916@gmail.com>
2021-09-07 22:22:50 +0900

    implement init

--- main.rs
+++ main.rs
@@ -1,8 +1,43 @@
 use std::env::{args};
 use std::process::{exit};
+use std::fs::{create_dir, File};
+use std::path::{Path};
+use std::io::{BufWriter, Write};
 
 fn print_usage(args: &Vec<String>) {
     eprintln!("Usage: {:} subcommand", args[0])
+}
+
+fn do_init() {
+    let path_to_init = Path::new(".");
+    let git_dir = path_to_init.join(".git");
+
+    // create .git/ directory
+    create_dir(&git_dir).unwrap_or_else(|_| {
+        println!("already initialized");
+        return;
+    });
+
+    // create .git/HEAD file
+    let head_file = git_dir.join("HEAD");
+    let mut f = BufWriter::new(File::create(&head_file).unwrap());
+    f.write(b"ref: refs/heads/master\n").unwrap();
+
+    // create .git/objects/ directory
+    let objects_dir = git_dir.join("objects");
+    create_dir(&objects_dir).unwrap();
+
+    // create .git/refs/ directory
+    let refs_dir = git_dir.join("refs");
+    create_dir(&refs_dir).unwrap();
+
+    // create .git/refs/heads/ directory
+    let refs_heads_dir = refs_dir.join("heads");
+    create_dir(&refs_heads_dir).unwrap();
+
+    // create .git/refs/tags/ directory
+    let refs_tags_dir = refs_dir.join("tags");
+    create_dir(&refs_tags_dir).unwrap();
 }
 
 fn main() {
@@ -17,7 +52,7 @@ fn main() {
 
     match subcommand.as_str() {
         "init" => {
-            println!("init!!!");
+            do_init();
         },
         "add" => {
             println!("add!!!");

e0cd0f5cfffd78d52aebb4a97f883a1f9c5fff97
Shuhei <sh7916@gmail.com>
2021-09-11 22:10:28 +0900

    オブジェクトのhashからオブジェクトファイルパスを求めるロジックを関数化した

--- src/api/objects.rs
+++ src/api/objects.rs
@@ -1,7 +1,7 @@
 use std::io::{BufWriter};
 use std::io::prelude::*;
 use std::fs::{OpenOptions, create_dir};
-use std::path::{Path};
+use std::path::{Path, PathBuf};
 use flate2::Compression;
 use flate2::write::ZlibEncoder;
 use sha1::{Sha1, Digest};
@@ -11,6 +11,17 @@ const OBJECTS_DIR: &str = "git2/objects/";
 pub struct ObjectWriter {
     encoder: ZlibEncoder<Vec<u8>>,
     hasher: Sha1,
+}
+
+fn get_object_path(hash: &str) -> PathBuf {
+    let hash1 = &hash[0..2];
+    let hash2 = &hash[2..];
+
+    let hash_dir = Path::new(OBJECTS_DIR).join(Path::new(hash1));
+    create_dir(&hash_dir).unwrap();
+    let object_file = hash_dir.join(Path::new(hash2));
+
+    object_file
 }
 
 impl ObjectWriter {
@@ -30,17 +41,12 @@ impl ObjectWriter {
         let mut compressed_bytes = self.encoder.finish().unwrap();
         let res = self.hasher.finalize();
 
-        let hex_hash = format!("{:x}", res);
-        let hash1 = &hex_hash[0..2];
-        let hash2 = &hex_hash[2..];
-
-        let hash_dir = Path::new(OBJECTS_DIR).join(Path::new(hash1));
-        create_dir(&hash_dir).unwrap();
-        let object_file = hash_dir.join(Path::new(hash2));
-        let mut f = BufWriter::new(OpenOptions::new().write(true).create(true).open(object_file).unwrap());
+        let hash = format!("{:x}", res);
+        let object_path = get_object_path(&hash);
+        let mut f = BufWriter::new(OpenOptions::new().write(true).create(true).open(object_path).unwrap());
         f.write_all(compressed_bytes.by_ref()).unwrap();
 
-        hex_hash
+        hash
     }
     }
 }

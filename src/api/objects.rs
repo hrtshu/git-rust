5add9f0c3fc0a28f438433374d1066e1bca79447
Shuhei <sh7916@gmail.com>
2021-09-14 20:57:16 +0900

    ObjectBaseを定義

--- src/api/objects.rs
+++ src/api/objects.rs
@@ -1,3 +1,4 @@
 pub mod io;
+pub mod base;
 pub mod blob;
 pub mod tree;
---
+++ src/api/objects/base.rs
@@ -0,0 +1,19 @@
+use std::io::Write;
+
+pub trait ObjectBase {
+    fn obj_type(&self) -> &str;
+
+    fn write_to<W>(&self, writer: &mut W) -> std::io::Result<()> where W: Write {
+        self.write_header_to(writer)?;
+        self.write_body_to(writer)?;
+        Ok(())
+    }
+
+    fn write_header_to<W>(&self, writer: &mut W) -> std::io::Result<()> where W: Write {
+        write!(writer, "{} {}\x00", self.obj_type(), self.body_size())
+    }
+
+    fn body_size(&self) -> usize;
+
+    fn write_body_to<W>(&self, writer: &mut W) -> std::io::Result<()> where W: Write;
+}
